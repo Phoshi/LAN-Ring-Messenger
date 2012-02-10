9afcc186b03a58d03907f863d9de1e8ae004c179
Phoshi <AHPhoshi@gmail.com>
2012-02-10 10:19:04 +0000

    Simple string extension methods to make life easier

---
+++ RingLAN/stringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Extensions {
+    public static class stringExtensions {
+        public static int Width(this string str, Font withFont) {
+            int width = TextRenderer.MeasureText(str, withFont).Width;
+            return width;
+        }
+
+        public static int Height(this string str, Font withFont) {
+            int height = TextRenderer.MeasureText(str, withFont).Height;
+            return height;
+        }
+
+        public static string With(this string str, params object[] formatWith) {
+            return String.Format(str, formatWith);
+        }
+
+        public static bool Glob(this string str, string match) {
+            match = Regex.Escape(match).Replace(@"\*", ".*").Replace(@"\?", ".");
+            return Regex.IsMatch(str, match, RegexOptions.IgnoreCase);
+        }
+    }
+}
