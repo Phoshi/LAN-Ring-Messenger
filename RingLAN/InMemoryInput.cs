384cfa701e398463311534e3cdabbf5f902f4b46
Phoshi <AHPhoshi@gmail.com>
2012-02-10 10:18:10 +0000

    InMemoryInput class overrides the low-level sending and recieving code in COMInput to provide a purely virtual interface

---
+++ RingLAN/InMemoryInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RingLAN {
+    class InMemoryInput : COMInput {
+        private List<byte> _buffer = new List<byte>();
+
+        public InMemoryInput(string dummyName) {
+            
+        }
+
+        public override byte getChar() {
+            while (_buffer.Count == 0) {
+                Thread.Sleep(100);
+            }
+            byte firstByte = _buffer[0];
+            _buffer.RemoveAt(0);
+            return firstByte;
+        }
+
+        public override void putChars(byte[] toWrite) {
+            foreach (byte b in toWrite) {
+                Console.Write((char)b);
+            }
+            _buffer.AddRange(toWrite);
+        }
+    }
+}
