833202b3e7e9b03833f4b818eb37a6a8153c7bec
Phoshi <AHPhoshi@gmail.com>
2012-02-10 10:20:11 +0000

    Implemented some low-level packet details

--- RingLAN/Message.cs
+++ RingLAN/Message.cs
@@ -11,10 +11,19 @@ namespace RingLAN {
         //
         //Class variables
         //
-        private int _to = -1;
-        private int _from = -1;
+        private char _to;
+        private char _from;
         private string _message = "";
         private MessageType _type;
+        private byte _checksum = 0;
+        private Dictionary<MessageType, char> _messageTypes = new Dictionary<MessageType, char>() {
+                                                                                                      {MessageType.Login, 'L'},
+                                                                                                      {MessageType.Logout, 'X'}, 
+                                                                                                      {MessageType.IdentResponse, 'R'},
+                                                                                                      {MessageType.Message, 'D'},
+                                                                                                      {MessageType.Acknowledge, 'Y'},
+                                                                                                      {MessageType.NotAcknowledge, 'N'},
+    };
 
         /// <summary>
         /// Property accessor for the message type
@@ -22,6 +31,33 @@ namespace RingLAN {
         public MessageType Type {
             get { return _type; }
         }
+
+        /// <summary>
+        /// Accessor for the packet's checksum
+        /// </summary>
+        public byte Checksum {
+            get {
+                if (_checksum == 0) {
+                    return MessageChecker.GetChecksum(this);
+                }
+                return _checksum;
+            }
+        }
+
+        /// <summary>
+        /// Gets the string representing the client which sent the message
+        /// </summary>
+        public string Sender {
+            get { return new string(_from, 1); }
+        }
+
+        /// <summary>
+        /// Gets the packet payload
+        /// </summary>
+        public string Payload {
+            get { return _message; }
+        }
+
         //
         //Constructors
         //
@@ -31,9 +67,52 @@ namespace RingLAN {
         /// <param name="message">The message text</param>
         /// <param name="recipient">The client ID to send to</param>
         /// <param name="type">The type of message to send</param>
-        public Message(string message, int recipient, MessageType type) {
+        public Message(string message, char recipient, MessageType type) {
             _message = message;
             _to = recipient;
+            _type = type;
+
+            if (type == MessageType.Login) {
+                _from = recipient;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new message object from the given packet.
+        /// </summary>
+        /// <param name="data">A byte array containing the packet data.</param>
+        public Message(byte[] data) {
+            
+        }
+
+        /// <summary>
+        /// Returns the packet data for this packet
+        /// </summary>
+        /// <returns>A byte array representing the packet</returns>
+        public byte[] ToByteArray(bool computeChecksum = true) {
+            /* Packet Structure
+             * {
+             *  Destination (Char)
+             *  Source (Char)
+             *  Type (Char)
+             *  Payload (10 Chars)
+             *  Checksum (Byte)
+             * }
+             */
+
+            byte[] byteArray = new byte[16];
+            StringBuilder header = new StringBuilder();
+            header.Append("{");
+            header.Append(_to);
+            header.Append(_from);
+            header.Append(_messageTypes[_type]);
+            byte[] headerBytes = ASCIIEncoding.ASCII.GetBytes(header.ToString());
+            Array.Copy(headerBytes, byteArray, headerBytes.Length);
+            byte[] payloadBytes = ASCIIEncoding.ASCII.GetBytes(_message);
+            Array.Copy(payloadBytes, 0, byteArray, 4, Math.Min(payloadBytes.Length, 10));
+            byteArray[14] = computeChecksum ? MessageChecker.GetChecksum(this) : (byte)0;
+            byteArray[15] = ASCIIEncoding.ASCII.GetBytes("}")[0];
+            return byteArray;
         }
 
         /// <summary>
@@ -41,7 +120,7 @@ namespace RingLAN {
         /// </summary>
         /// <returns></returns>
         public override string ToString() {
-            return base.ToString();
+            return _message;
         }
     }
 }
