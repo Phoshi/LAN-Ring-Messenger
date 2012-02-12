2ad93c91dc597c5d1127ad29d6bda342d754dc96
Phoshi <AHPhoshi@gmail.com>
2012-02-12 01:42:52 +0000

    Implemented message handling and passing functionality

--- RingLAN/Client.cs
+++ RingLAN/Client.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Extensions;
 
 namespace RingLAN {
     public class Client {
@@ -13,16 +14,66 @@ namespace RingLAN {
         /// Initialised to -1 because this needs to be set up when the LAN ring is turned on, not the client!
         /// </summary>
         private char _address = ' ';
-        public char Address { get { return _address; }
+
+        private bool _promiscuous = false;
+
+        private bool _loggedIn = false;
+
+        private List<char> _identifiedUsers = new List<char>();
+
+        public bool Debug {
+            get { return _promiscuous; }
+            set { _promiscuous = value; }
+        }
+
+        /// <summary>
+        /// Return a list of known clients
+        /// </summary>
+        public List<char> Clients {
+            get { return _identifiedUsers; }
+        }
+
+        /// <summary>
+        /// Gets and sets the client's address.
+        /// </summary>
+        public char Address {
+            get { return _address; }
             set {
-                _comms.Send(new Message(value.ToString(), value, MessageType.Login));
+                _comms.Send(new Message(null, value, value, MessageType.Login));
+                _address = value;
             }
+        }
+
+        /// <summary>
+        /// Returns whether the client is logged in
+        /// </summary>
+        public bool LoggedIn {
+            get { return _loggedIn; }
+            set {
+                if (!value) {
+                    _comms.PassOn(new Message(null, Address, Address, MessageType.Logout));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets an address suitable for display
+        /// </summary>
+        public string DisplayAddress {
+            get { return new string(Address, 1); }
         }
 
         /// <summary>
         /// The comms object (implementing ICommunication) to be used for communication in this object
         /// </summary>
         private ICommunication _comms = null;
+
+        /// <summary>
+        /// Gets the ICommunication object implementing communications
+        /// </summary>
+        public ICommunication Communications {
+            get { return _comms; }
+        }
 
         //
         //Constructors
@@ -32,7 +83,18 @@ namespace RingLAN {
         /// </summary>
         /// <param name="comms">The interface (implementing ICommunication) that will handle sending and recieving messages</param>
         public Client(ICommunication comms) {
+            Logger.Log("New Client starting up.");
             _comms = comms;
+            comms.Parent = this;
+            comms.Recieved += RecieveMessage;
+        }
+
+        //
+        // Public Methods
+        //
+
+        public void Close() {
+            _comms.Close();
         }
 
         //
@@ -75,13 +137,55 @@ namespace RingLAN {
         /// <param name="args">The MessageEventArgs object representing the message to send</param>
         public void SendMessage(object sender, MessageEventArgs args) {
             _comms.Send(args.Message);
+            if (args.Message.Address != Address) {
+                OnActionableMessageRecieved(args);
+            }
         }
 
         public void RecieveMessage(object sender, MessageEventArgs args) {
             Message message = args.Message;
-            if (message.Type == MessageType.Message) {
+            if (_promiscuous || message.Address == Address || message.SenderAddress == Address) {
+                if (message.Type == MessageType.Message) {
+                    OnActionableMessageRecieved(args);
+                }
+            }
+            if (message.Type == MessageType.Login) {
+                if (message.SenderAddress == Address) {
+                    if (LoggedIn) {
+                        _comms.PassOn(message.Acknowledge);
+                    }
+                    else {
+                        _loggedIn = true;
+                    }
+                }
+
+                if (!_identifiedUsers.Contains(message.Address)) {
+                    _identifiedUsers.Add(message.Address);
+                }
+                _comms.PassOn(new Message(null, message.Address, this.Address, MessageType.IdentResponse));
                 OnActionableMessageRecieved(args);
             }
+            if (message.Type == MessageType.Logout) {
+                if (message.Address == Address) {
+                    _loggedIn = false;
+                    _address = ' ';
+                }
+                _identifiedUsers.Remove(message.Address);
+                OnActionableMessageRecieved(args);
+            }
+            if (message.Type == MessageType.IdentResponse) {
+                if (!_identifiedUsers.Contains(message.SenderAddress)) {
+                    _identifiedUsers.Add(message.SenderAddress);
+                }
+                OnActionableMessageRecieved(args);
+            }
+            if (message.Type == MessageType.Acknowledge) {
+                if (!LoggedIn) {
+                    _address = ' ';
+                    OnActionableMessageRecieved(args);
+                }
+            }
+
         }
     }
 }
