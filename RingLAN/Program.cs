1382c3a3f0ffadfe147bbc5d998b831685640cca
Phoshi <AHPhoshi@gmail.com>
2012-02-12 01:45:03 +0000

    Now starts a startup config form

--- RingLAN/Program.cs
+++ RingLAN/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace RingLAN {
@@ -12,7 +13,8 @@ namespace RingLAN {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ClientUI());
+            Logger.Log("Startup.");
+            Application.Run(new Startup());
         }
     }
 }
