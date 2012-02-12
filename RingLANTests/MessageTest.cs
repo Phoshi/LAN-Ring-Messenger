3ca48d57aa2ca261e0edecdf708dc7d031f13e4a
Phoshi <AHPhoshi@gmail.com>
2012-02-12 01:46:46 +0000

    Tests

---
+++ RingLANTests/MessageTest.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+using Extensions;
+using RingLAN;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RingLANTests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for MessageTest and is intended
+    ///to contain all MessageTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class MessageTest {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext {
+            get {
+                return testContextInstance;
+            }
+            set {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Message Constructor
+        ///</summary>
+        [TestMethod()]
+        public void MessageConstructorTest() {
+            string testPacket = "{TFD1234567890\0}";
+            byte[] data = ASCIIEncoding.ASCII.GetBytes(testPacket);
+            Message target = new Message(data);
+            string laterData = ASCIIEncoding.ASCII.GetString(target.ToByteArray(false));
+            if (laterData != testPacket) {
+                Assert.Fail("Packets are different: {0} != {1}".With(laterData, testPacket));
+            }
+        }
+    }
+}
---
+++ RingLANTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RingLANTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RingLANTests")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("caca48e1-6298-4b4b-a706-a157345415f1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
