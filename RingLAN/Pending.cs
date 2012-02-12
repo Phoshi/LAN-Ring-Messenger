c32a1c3e2c47caf7898179f91b3c512bac8c087b
Phoshi <AHPhoshi@gmail.com>
2012-02-12 01:46:00 +0000

    Added Pending class for pending table

---
+++ RingLAN/Pending.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RingLAN {
+    public class Pending {
+        public Message Message;
+        public DateTime LastSend;
+        public int SendCount;
+
+        public Pending(Message message) {
+            Message = message;
+            LastSend = DateTime.MinValue;
+            SendCount = 0;
+        }
+    }
+}
