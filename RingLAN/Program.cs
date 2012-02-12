120e8cc56492c09a0ad238ad0238fd823d404314
Phoshi <AHPhoshi@gmail.com>
2012-02-12 14:14:20 +0000

    Added basic banner

--- RingLAN/Program.cs
+++ RingLAN/Program.cs
@@ -5,6 +5,12 @@ using System.Threading;
 using System.Windows.Forms;
 
 namespace RingLAN {
+    /// <summary>
+    /// Author Name:    Andrew Heaford
+    /// Date:           2012/02/12
+    /// Description:    IM Client that supports communications over a serial ring network.
+    /// Advice:         Try not to die.
+    /// </summary>
     static class Program {
         /// <summary>
         /// The main entry point for the application.
