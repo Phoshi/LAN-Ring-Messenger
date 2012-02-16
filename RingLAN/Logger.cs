9b91b35a8103e71d4bbd9596ca4ef26d4fd87f1b
Phoshi <AHPhoshi@gmail.com>
2012-02-16 13:27:00 +0000

    Logger now supports logging bytes to a seperate file

--- RingLAN/Logger.cs
+++ RingLAN/Logger.cs
@@ -4,9 +4,11 @@ using Extensions;
 namespace RingLAN {
     static class Logger {
         private const string path = "output.log";
+        private const string rawPath = "packets.log";
 
         static Logger() {
             File.Delete(path);
+            File.Delete(rawPath);
         }
 
         public static void Log(string text, string section = null) {
@@ -19,6 +21,20 @@ namespace RingLAN {
                 logger.Close();
             }
             catch (IOException) {
+                //Logger failed, better log that.
+                //Wait...
+            }
+        }
+        public static void Log(byte rawByte)
+        {
+            try
+            {
+                StreamWriter logger = new StreamWriter(rawPath, true);
+                logger.Write((char)rawByte);
+                logger.Close();
+            }
+            catch (IOException)
+            {
                 //Logger failed, better log that.
                 //Wait...
             }
